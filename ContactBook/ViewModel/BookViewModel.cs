777508b9e3461c672d36e0550d43237ebdf994bc
hickee032 <hickee32@gmail.com>
2023-11-22 17:00:00 +0900

    ContactBook

--- ContactBook/ViewModel/BookViewModel.cs
+++ ContactBook/ViewModel/BookViewModel.cs
@@ -4,10 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ContactBook.ViewModel
 {
     public class BookViewModel : ObservableObject
     {
+        private ContactsViewModel _contactsVM;
+        public ContactsViewModel ContactsVM {
+            get { return _contactsVM; }
+            set { OnPropertyChanged(ref _contactsVM, value); }
+        }
+        public ICommand LoadContactsCommand { get; private set; }
+        public ICommand LoadFavoritesCommand { get; private set; }
+        public BookViewModel() {
+            ContactsVM= new ContactsViewModel();
+
+            LoadContactsCommand = new RelayCommand(LoadContacts);
+            LoadFavoritesCommand= new RelayCommand(LoadFavorites);
+        }
+
+        private void LoadContacts() {
+
+        }
+        private void LoadFavorites() {
+
+        }
     }
 }
--- ContactBook/ViewModel/ContactsViewModel.cs
+++ ContactBook/ViewModel/ContactsViewModel.cs
@@ -1,6 +1,8 @@
-﻿using ContactBook.Utility;
+﻿using ContactBook.Model;
+using ContactBook.Utility;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,20 @@ namespace ContactBook.ViewModel
 {
     public class ContactsViewModel : ObservableObject
     {
+        private Contact _selectedContact;
+        public Contact SelectedContact {
+            get { return _selectedContact; }
+            set { OnPropertyChanged(ref _selectedContact, value); }
+        }
+
+        public ObservableCollection<Contact> Contacts { get; private set; }
+        public ContactsViewModel() {
+
+        }
+        public void LoadContacts(IEnumerable<Contact> contacts) {
+            Contacts = new ObservableCollection<Contact>(contacts);
+            OnPropertyChanged("Contacts");
+        }
+
     }
 }
