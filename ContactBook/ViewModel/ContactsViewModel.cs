9e0f06fcada44ed9897933669f211ef6cd585f61
hickee032 <hickee32@gmail.com>
2023-11-28 17:00:00 +0900

    ContactBook

--- ContactBook/ViewModel/ContactsViewModel.cs
+++ ContactBook/ViewModel/ContactsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ContactBook.Model;
+using ContactBook.Services;
 using ContactBook.Utility;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ContactBook.ViewModel
 {
@@ -17,10 +19,58 @@ namespace ContactBook.ViewModel
             set { OnPropertyChanged(ref _selectedContact, value); }
         }
 
-        public ObservableCollection<Contact> Contacts { get; private set; }
-        public ContactsViewModel() {
+        private bool _isEditMode;
+        public bool IsEditMode {
+            get { return _isEditMode; }
+            set {
+                OnPropertyChanged(ref _isEditMode, value);
+                OnPropertyChanged("IsDisplayMode");
+            }
+        }
+        public bool IsDisplayMode {
+            get { return !_isEditMode; }
+        }
+
+        public ObservableCollection<Contact> Contacts { get; set; }
+
+        public ICommand EditCommand { get; private set; }
+        public ICommand SaveCommand { get; private set; }
+        public ICommand UpdateCommand { get; private set; }
+
+        private IContactDataService _dataService;
+
+        public ContactsViewModel(IContactDataService dataService) {
+            _dataService = dataService;
+                
+            EditCommand = new RelayCommand(Edit, CanEdit);
+            SaveCommand = new RelayCommand(Save, IsEdit);
+            UpdateCommand = new RelayCommand(Update);
 
         }
+        private void Update() {
+            _dataService.Save(Contacts);
+        }
+
+        private void Save() {
+            _dataService.Save(Contacts);
+            IsEditMode = false;
+            OnPropertyChanged("SelectedContact");
+        }
+        private bool IsEdit()
+        {
+            return IsEditMode;
+        }
+        private bool CanEdit() {
+            if (SelectedContact == null)
+                return false;
+
+            return !IsEditMode;
+        }
+        private void Edit()
+        {
+            IsEditMode = true;
+        }
+
         public void LoadContacts(IEnumerable<Contact> contacts) {
             Contacts = new ObservableCollection<Contact>(contacts);
             OnPropertyChanged("Contacts");
