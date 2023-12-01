b6c25bbbf76a35dd1d2833d59e43eecb984dc0e2
hickee032 <hickee32@gmail.com>
2023-12-01 15:00:00 +0900

    ContactBook

---
+++ ContactBook/Services/JsonContactDataService.cs
@@ -0,0 +1,20 @@
+﻿using ContactBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.Services
+{
+    public class JsonContactDataService : IContactDataService
+    {
+        public IEnumerable<Contact> GetContacts() {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IEnumerable<Contact> contacts) {
+            throw new NotImplementedException();
+        }
+    }
+}
