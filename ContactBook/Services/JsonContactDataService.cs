5db29b9e766ca8645560612bef0caa79dc43baa3
hickee032 <hickee32@gmail.com>
2023-12-02 15:00:00 +0900

    ContactBook

--- ContactBook/Services/JsonContactDataService.cs
+++ ContactBook/Services/JsonContactDataService.cs
@@ -1,6 +1,8 @@
 ﻿using ContactBook.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,25 @@ namespace ContactBook.Services
 {
     public class JsonContactDataService : IContactDataService
     {
+        private readonly string _dataPath = "Resources\\contactdata.json";
         public IEnumerable<Contact> GetContacts() {
-            throw new NotImplementedException();
+            if (!File.Exists(_dataPath)) {
+                File.Create(_dataPath).Close();
+            }
+
+            var serializedContacts = File.ReadAllText(_dataPath);
+            var contacts = JsonConvert.DeserializeObject<IEnumerable<Contact>>(serializedContacts);
+
+            if (contacts == null) {
+                return new List<Contact>();
+            }
+            return contacts;
         }
 
         public void Save(IEnumerable<Contact> contacts) {
-            throw new NotImplementedException();
+            
+            var serializedContacts = JsonConvert.SerializeObject(contacts);
+            File.WriteAllText(_dataPath, serializedContacts);
         }
     }
 }
